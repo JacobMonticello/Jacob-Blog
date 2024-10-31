d4d5e10d46cbb0dd7264800965f48bafb7eca7ee
Jacob monticello <Jacob Monticello>
2024-10-31 19:14:14 -0400

    Initial commit from Jacob

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	//base: '/JacobBlog/', // GitHub repository name
 	integrations: [mdx(), sitemap()],
 });
---
+++ src/content/blog/new-post.md
@@ -0,0 +1,8 @@
+---
+title: 'Test post'
+description: 'Lorem ipsum dolor sit amet'
+pubDate: 'Oct 15 2024'
+heroImage: '/blog-placeholder-4.jpg'
+---
+
+Test
